d010ec1483e503be54dec605fe383ea032ac768c
xleofarias <leofarias.bliz@gmail.com>
2026-09-01 23:17:14 -0300

    fix: validate account number on creation

--- Account.cs
+++ Account.cs
@@ -1,33 +1,14 @@
 public class Account
 {
     public Guid Id { get;} = Guid.NewGuid();
-    public int AccountNumber { get; set; } 
+    public string AccountNumber { get; private set; } 
     public required string AccountHolder { get; set; }
     public decimal Balance { get; private set; } = 0;
     public DateTime CreatedAt { get; } = DateTime.UtcNow;
-    public string Status { get; set; } = "Active";
-    public void Deposit(decimal amount)
+    public string Status { get; private set; } = "Active";
+
+    public Account(string accountNumber)
     {
-        if (amount <= 0)
-        {
-            throw new ArgumentException("Deposit amount must be positive.");
-        }
-
-        Balance += amount;
-    }
-
-    public void Withdraw(decimal amount)
-    {
-        if (amount <= 0)
-        {
-            throw new ArgumentException("Withdrawal amount must be positive.");
-        }
-
-        if (amount > Balance)
-        {
-            throw new InvalidOperationException("Insufficient funds.");
-        }
-
-        Balance -= amount;
+        AccountNumber = accountNumber;
     }
 }
