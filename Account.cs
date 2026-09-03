406ee943b9632e3f995860ff708299f9af50cdc8
xleofarias <leofarias.bliz@gmail.com>
2026-09-03 13:55:15 -0300

    deleted deposit and withdraw

--- Account.cs
+++ Account.cs
@@ -4,9 +4,8 @@ public class Account
 {
     public Guid Id { get;} = Guid.NewGuid();
     public string AccountNumber { get; private set; } 
-    public required string AccountHolder { get; set; }
+    public string? AccountHolder { get; set; }
     public decimal Balance { get; private set; } = 0;
-    public DateTime CreatedAt { get; } = DateTime.UtcNow;
     public string Status { get; private set; } = "Active";
 
     public Account(string accountNumber)
@@ -24,30 +23,5 @@ public class Account
         }
 
         AccountNumber = accountNumber;
-    }
-
-    public void Deposit(decimal amount)
-    {
-        if (amount <= 0)
-        {
-            throw new ArgumentException("Deposit amount must be positive.");
-        }
-
-        Balance += amount;
-    }
-
-    public void Withdraw(decimal amount)
-    {
-        if (amount <= 0)
-        {
-            throw new ArgumentException("Withdrawal amount must be positive.");
-        }
-
-        if (amount > Balance)
-        {
-            throw new InvalidOperationException("Insufficient funds.");
-        }
-
-        Balance -= amount;
     }
 }
