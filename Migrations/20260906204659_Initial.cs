f141e4fb84a68b62e950c3389ead3c3c2238356d
xleofarias <leofarias.bliz@gmail.com>
2026-09-06 19:13:20 -0300

    feat: create DbContext and migrations

---
+++ Migrations/20260906204659_Initial.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LedgerAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Accounts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AccountNumber = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    AccountHolder = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Balance = table.Column<decimal>(type: "decimal(18,2)", precision: 18, scale: 2, nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Accounts", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Accounts");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using DotNetEnv;
+
+// Load environment variables from .env file
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+// Add DbContext
+var connectionString = Environment.GetEnvironmentVariable("ConnectionSqlServer");
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(connectionString ?? throw new InvalidOperationException("Connection string not found.")));
 
 var app = builder.Build();
 
